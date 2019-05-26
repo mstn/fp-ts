898c80350776f36ae5df3ab368fa6e5b2f95d5f8
gcanti <giulio.canti@gmail.com>
2019-05-26 10:09:30 +0200

    augment Identity

--- docs/modules/Identity.ts.md
+++ docs/modules/Identity.ts.md
@@ -11,9 +11,21 @@ parent: Modules
 - [Identity (type alias)](#identity-type-alias)
 - [URI (type alias)](#uri-type-alias)
 - [URI (constant)](#uri-constant)
+- [alt (constant)](#alt-constant)
+- [extract (constant)](#extract-constant)
 - [getEq (constant)](#geteq-constant)
 - [getShow (constant)](#getshow-constant)
 - [identity (constant)](#identity-constant)
+- [of (constant)](#of-constant)
+- [ap (function)](#ap-function)
+- [chain (function)](#chain-function)
+- [extend (function)](#extend-function)
+- [foldMap (function)](#foldmap-function)
+- [map (function)](#map-function)
+- [reduce (function)](#reduce-function)
+- [reduceRight (function)](#reduceright-function)
+- [sequence (function)](#sequence-function)
+- [traverse (function)](#traverse-function)
 
 ---
 
@@ -47,6 +59,26 @@ export const URI = ...
 
 Added in v2.0.0
 
+# alt (constant)
+
+**Signature**
+
+```ts
+export const alt: Alt1<URI>['alt'] = ...
+```
+
+Added in v2.0.0
+
+# extract (constant)
+
+**Signature**
+
+```ts
+export const extract: Comonad1<URI>['extract'] = ...
+```
+
+Added in v2.0.0
+
 # getEq (constant)
 
 **Signature**
@@ -73,6 +105,111 @@ Added in v2.0.0
 
 ```ts
 export const identity: Monad1<URI> & Foldable1<URI> & Traversable1<URI> & Alt1<URI> & Comonad1<URI> & ChainRec1<URI> = ...
+```
+
+Added in v2.0.0
+
+# of (constant)
+
+**Signature**
+
+```ts
+export const of: Monad1<URI>['of'] = ...
+```
+
+Added in v2.0.0
+
+# ap (function)
+
+**Signature**
+
+```ts
+export const ap: Monad1<URI>['ap'] = (mab, ma) => ...
+```
+
+Added in v2.0.0
+
+# chain (function)
+
+**Signature**
+
+```ts
+export const chain: Monad1<URI>['chain'] = (ma, f) => ...
+```
+
+Added in v2.0.0
+
+# extend (function)
+
+**Signature**
+
+```ts
+export const extend: Comonad1<URI>['extend'] = (wa, f) => ...
+```
+
+Added in v2.0.0
+
+# foldMap (function)
+
+**Signature**
+
+```ts
+export const foldMap: Foldable1<URI>['foldMap'] = _ => (fa, f) => ...
+```
+
+Added in v2.0.0
+
+# map (function)
+
+**Signature**
+
+```ts
+export const map: Monad1<URI>['map'] = (ma, f) => ...
+```
+
+Added in v2.0.0
+
+# reduce (function)
+
+**Signature**
+
+```ts
+export const reduce: Foldable1<URI>['reduce'] = (fa, b, f) => ...
+```
+
+Added in v2.0.0
+
+# reduceRight (function)
+
+**Signature**
+
+```ts
+export const reduceRight: Foldable1<URI>['reduceRight'] = (fa, b, f) => ...
+```
+
+Added in v2.0.0
+
+# sequence (function)
+
+**Signature**
+
+```ts
+export const sequence: Traversable1<URI>['sequence'] = <F>(F: Applicative<F>) => <A>(
+  ta: Identity<HKT<F, A>>
+): HKT<F, Identity<A>> => ...
+```
+
+Added in v2.0.0
+
+# traverse (function)
+
+**Signature**
+
+```ts
+export const traverse: Traversable1<URI>['traverse'] = <F>(F: Applicative<F>) => <A, B>(
+  ta: Identity<A>,
+  f: (a: A) => HKT<F, B>
+): HKT<F, Identity<B>> => ...
 ```
 
 Added in v2.0.0
--- src/Identity.ts
+++ src/Identity.ts
@@ -10,6 +10,7 @@ import { Monad1 } from './Monad'
 import { Eq } from './Eq'
 import { Show } from './Show'
 import { Traversable1 } from './Traversable'
+import { augment } from './augment'
 
 declare module './HKT' {
   interface URI2HKT<A> {
@@ -50,11 +51,72 @@ const chainRec = <A, B>(a: A, f: (a: A) => Either<A, B>): B => {
   return v.right
 }
 
-const traverse = <F>(F: Applicative<F>) => <A, B>(ta: Identity<A>, f: (a: A) => HKT<F, B>): HKT<F, Identity<B>> => {
+/**
+ * @since 2.0.0
+ */
+export const map: Monad1<URI>['map'] = (ma, f) => f(ma)
+
+/**
+ * @since 2.0.0
+ */
+export const of: Monad1<URI>['of'] = id
+
+/**
+ * @since 2.0.0
+ */
+export const ap: Monad1<URI>['ap'] = (mab, ma) => mab(ma)
+
+/**
+ * @since 2.0.0
+ */
+export const chain: Monad1<URI>['chain'] = (ma, f) => f(ma)
+
+/**
+ * @since 2.0.0
+ */
+export const alt: Alt1<URI>['alt'] = id
+
+/**
+ * @since 2.0.0
+ */
+export const extract: Comonad1<URI>['extract'] = id
+
+/**
+ * @since 2.0.0
+ */
+export const extend: Comonad1<URI>['extend'] = (wa, f) => f(wa)
+
+/**
+ * @since 2.0.0
+ */
+export const reduce: Foldable1<URI>['reduce'] = (fa, b, f) => f(b, fa)
+
+/**
+ * @since 2.0.0
+ */
+export const foldMap: Foldable1<URI>['foldMap'] = _ => (fa, f) => f(fa)
+
+/**
+ * @since 2.0.0
+ */
+export const reduceRight: Foldable1<URI>['reduceRight'] = (fa, b, f) => f(fa, b)
+
+/**
+ * @since 2.0.0
+ */
+export const traverse: Traversable1<URI>['traverse'] = <F>(F: Applicative<F>) => <A, B>(
+  ta: Identity<A>,
+  f: (a: A) => HKT<F, B>
+): HKT<F, Identity<B>> => {
   return F.map(f(ta), id)
 }
 
-const sequence = <F>(F: Applicative<F>) => <A>(ta: Identity<HKT<F, A>>): HKT<F, Identity<A>> => {
+/**
+ * @since 2.0.0
+ */
+export const sequence: Traversable1<URI>['sequence'] = <F>(F: Applicative<F>) => <A>(
+  ta: Identity<HKT<F, A>>
+): HKT<F, Identity<A>> => {
   return F.map(ta, id)
 }
 
@@ -63,17 +125,53 @@ const sequence = <F>(F: Applicative<F>) => <A>(ta: Identity<HKT<F, A>>): HKT<F, 
  */
 export const identity: Monad1<URI> & Foldable1<URI> & Traversable1<URI> & Alt1<URI> & Comonad1<URI> & ChainRec1<URI> = {
   URI,
-  map: (ma, f) => f(ma),
-  of: id,
-  ap: (mab, ma) => mab(ma),
-  chain: (ma, f) => f(ma),
-  reduce: (fa, b, f) => f(b, fa),
-  foldMap: _ => (fa, f) => f(fa),
-  reduceRight: (fa, b, f) => f(fa, b),
+  map,
+  of,
+  ap,
+  chain,
+  reduce,
+  foldMap,
+  reduceRight,
   traverse,
   sequence,
-  alt: id,
-  extract: id,
-  extend: (wa, f) => f(wa),
+  alt,
+  extract,
+  extend,
   chainRec
+}
+
+const {
+  alt$,
+  ap$,
+  apFirst,
+  apFirst$,
+  apSecond,
+  apSecond$,
+  chain$,
+  chainFirst,
+  chainFirst$,
+  duplicate,
+  extend$,
+  foldMap$,
+  map$,
+  reduce$,
+  reduceRight$
+} = augment(identity)
+
+export {
+  alt$,
+  ap$,
+  apFirst,
+  apFirst$,
+  apSecond,
+  apSecond$,
+  chain$,
+  chainFirst,
+  chainFirst$,
+  duplicate,
+  extend$,
+  foldMap$,
+  map$,
+  reduce$,
+  reduceRight$
 }
